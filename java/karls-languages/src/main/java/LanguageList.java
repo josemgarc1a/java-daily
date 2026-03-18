072b54274b1266c17f6a9e3623acf8a9ab1ef06e
Jose Garcia <josemgarc1a@icloud.com>
2026-03-18 08:42:58 -0500

    solution: Karl's Languages

--- java/karls-languages/src/main/java/LanguageList.java
+++ java/karls-languages/src/main/java/LanguageList.java
@@ -2,33 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LanguageList {
+
+    private static final String KOTLIN = "Kotlin";
+    private static final String JAVA = "Java";
     private final List<String> languages = new ArrayList<>();
 
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Please implement the isEmpty() method");
+        return languages.isEmpty();
     }
 
     public void addLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the addLanguage() method");
+        languages.add(language);
     }
 
     public void removeLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the removeLanguage() method");
+        languages.remove(language);
     }
 
     public String firstLanguage() {
-        throw new UnsupportedOperationException("Please implement the firstLanguage() method");
+        return languages.get(0);
     }
 
     public int count() {
-        throw new UnsupportedOperationException("Please implement the count() method");
+        return languages.size();
     }
 
     public boolean containsLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the containsLanguage() method");
+        return languages.contains(language);
     }
 
     public boolean isExciting() {
-        throw new UnsupportedOperationException("Please implement the isExciting() method");
+        return containsLanguage(JAVA) || containsLanguage(KOTLIN);
     }
 }
